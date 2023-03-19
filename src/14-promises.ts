1f4f9a283bd7f10225ed5d99387ec1b37bf1193b
jahiker <rojasjahiker@gmail.com>
2023-03-19 02:01:06 -0400

    [add] promises type

---
+++ src/14-promises.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+(async () => {
+
+  function delay(time: number) {
+    const promise = new Promise<boolean>((res) => {
+      setTimeout(() => {
+        res(true);
+      }, time);
+    })
+    return promise;
+  }
+
+  function getProducts() {
+    const promise = axios.get("https://api.escuelajs.co/api/v1/products");
+    return promise;
+  }
+
+  async function getProductsAsync() {
+    const promise = await axios.get("https://api.escuelajs.co/api/v1/products");
+    return promise;
+  }
+
+  const rta = await delay(3000);
+  console.log(rta)
+
+  const products = await getProducts()
+  console.log(products.data)
+
+  const productsAsync = await getProductsAsync()
+  console.log(productsAsync.data)
+})();
